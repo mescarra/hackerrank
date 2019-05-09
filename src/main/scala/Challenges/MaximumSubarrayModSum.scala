47d6e1820353c55db091695ccde81b8c94319e23
mescarra@gmail.com <martin.escarra@jemstep.com>
2019-05-09 14:19:21 -0300

    max subarray mod sum

--- src/main/scala/Challenges/MaximumSubarrayModSum.scala
+++ src/main/scala/Challenges/MaximumSubarrayModSum.scala
@@ -1,5 +1,28 @@
 package Challenges
+import collection.mutable.TreeSet
+
+/*
+ * Maximum Subarray Modular Sum
+ * https://www.hackerrank.com/challenges/maximum-subarray-sum/problem
+ *
+ * Find maximum subarray modular sum in O(n lgn).
+ */
 
 object MaximumSubarrayModSum {
-
+  def maximumSum(a: Array[Long], m: Long): Long = {
+    var s = new TreeSet[Long]() // Red-Black Tree
+    var prefixSum = 0L
+    var max = 0L
+    for (i <- a.indices) {
+      prefixSum = (prefixSum + a(i))%m
+      if (prefixSum > max) max = prefixSum
+      s += prefixSum
+      val ls = s.iteratorFrom(prefixSum+1)
+      if (ls.hasNext) {
+        val contender = (prefixSum - ls.next + m)%m
+        if (contender > max) max = contender
+      }
+    }
+    max
+  }
 }
